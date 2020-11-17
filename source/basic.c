0852012d062d562638e205a0c6c41dd90bc3783e
Okvian <okedarkblue@gmail.com>
2020-11-17 22:15:57 +0800

    add file init.c and edit basic.c

---
+++ source/basic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+
+double get_det(double *initArray) {
+    double result;
+    int raw = sizeof(initArray) / sizeof(initArray[0]);
+    int column = sizeof(initArray)/row;
+    if (raw != column) result = 0.0;
+    else {
+        
+    }
+    return result;
+}
