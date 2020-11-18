ef9ad90e6b13dd852e63720d71722b4e4a594e93
Okvian <okedarkblue@gmail.com>
2020-11-18 22:14:40 +0800

    edit doing nothing

--- source/basic.c
+++ source/basic.c
@@ -4,13 +4,25 @@
 
 
 
-double get_det(double *initArray) {
-    double result;
-    int raw = sizeof(initArray) / sizeof(initArray[0]);
+double get_det(double *initArray[]) {
+    double result, pos, neg;
+    double *posarr, *negarr;
+    int row = sizeof(initArray) / sizeof(initArray[0]);
     int column = sizeof(initArray)/row;
-    if (raw != column) result = 0.0;
+    int tmp = 0;
+    
+    if (row != column) result = 0.0;
     else {
-        
+        for(int i = 0; i<row;i++) {
+            pos = 1.0;
+            neg = 1.0;
+            for(int j=0; j<column;j++) {
+                
+                pos = pos * initArray[tmp][tmp];
+            }
+            posarr[i] = pos;
+            negarr[i] = neg;
+        }
     }
     return result;
 }
