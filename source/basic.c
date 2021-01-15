9c8b16389b8fa5b4fafad9553eee91c659057a99
Okvian <okedarkblue@gmail.com>
2021-01-15 21:56:08 +0800

    fixing algorithm to get positive segment of the determinant

--- source/basic.c
+++ source/basic.c
@@ -1,48 +1,59 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+//#include <stdlib.h>
+//#include <math.h>
 
-double get_det(double *initArray);
+double A[5][5] = {
+                        {2,3,4,5,1},
+                        {6,7,8,9,2},
+                        {1,2,3,4,3},
+                        {5,6,7,8,4},
+                        {1,2,3,4,3}
+                    };
+double get_det(double *initArray,int therow, int thecol);
+
+int xx = sizeof(A)/sizeof(A[0]);
+int yy = sizeof(A[0])/sizeof(A[0][0]);
 
 int main() {
-    double A[4][4] = {
-                        {2,3,4,5},
-                        {6,7,8,9},
-                        {1,2,3,4},
-                        {5,6,7,8}
-                    };
-    double myresult = get_det(*A);
+    double myresult = get_det((double *) &A[0], xx, yy);
     printf("Hasil saya adalah %f\n",myresult);
 
     return 0;
 }
-double get_det(double *initArray) {
+double get_det(double *initArray, int therow, int thecol) {
     double result;
     double *posarr, *negarr;
     int ro, co;
-    int row = sizeof(initArray) / sizeof(initArray[0]);
-    int column = sizeof(initArray)/row;
-    double pos[column], neg[column];
+    double pos[therow], neg[therow];
     double tmp = 0.0;
 
-    if (row != column) result = 0;
+    for (int i=0;i<therow;i++) {
+        pos[i] = 1.0;
+        neg[i] = 1.0;
+    }
+
+    if (therow != thecol) result = 0;
     else {
         result = 0;
-        for(int idx = 0; idx<column; idx++ ) {
-                /* pos[0] = pos[0] * initArray[idx][idx]; */
-                tmp = *( *(initArray + idx) + idx);
+        for(int idx = 0; idx<therow; idx++ ) {
+                tmp = (double) ( *(initArray + (idx*therow)+idx));
                 pos[0] = pos[0] * tmp;
         }
-        for (int idx = 1; idx<column; idx++) {
+
+        for (int idx = 1; idx<therow; idx++) {
             co = idx;
-            for(ro=0; ro<column;ro++)
-                pos[idx] = pos[idx]*initArray[ro][co];
+            tmp = 1.0;
+            for(ro=0; ro<therow;ro++) {
+                tmp = (double) ( *(initArray + (ro*therow)+co));
+                pos[idx] = pos[idx] * tmp;
                 co = co + 1;
-                if (co>=column) co = 0;
+                if (co>=therow) co = 0;
+            }
         }
-        for(int i=0; i<column;i++) {
+        for(int i=0; i<therow;i++) {
             result = result + pos[i];
         }
+
     }
     return result;
 }
