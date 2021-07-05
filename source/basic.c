6dc319bc97c502785765cf09c7a82c2689c96cfd
Okvian <okedarkblue@gmail.com>
2021-07-05 22:16:24 +0800

    masih perlu edit untuk cari matrix size

--- source/basic.c
+++ source/basic.c
@@ -1,4 +1,4 @@
-//#include <stdio.h>
+#include <stdio.h>
 //#include <stdlib.h>
 //#include <math.h>
 
@@ -7,33 +7,48 @@
 * matrix [row][column]
 */
 
+typedef struct {
+	double deteminan;
+} Summary;
 
-double A[5][5] = {
-                        {2,3,4,5,1},
-                        {6,7,8,9,2},
-                        {1,2,3,4,3},
-                        {5,6,7,8,4},
-                        {1,2,3,4,3}
+double A[6][6] = {
+                        {2,3,4,5,1,2.4},
+                        {6,7,8,9,2,3.5},
+                        {1,2,3,4,3,3.2},
+                        {5,6,7,8,4,2.2},
+                        {1,2,3,4,3,3.3},
+                        {3,5,3,2,6,4.5}
                     };
 double matrix_det(double *initArray,int therow, int thecol);
 void matrix_print(double *initArray, int therow, int thecol);
 double *matrix_transpose(double *initArray, int therow, int thecol);
 double *matrix_identity(int therow, int thecol);
+double *matrix_gauss_jordan_elimination(double *initArray, int therow, int thecol);
+int *array_size(double *initArray);
 
-int xx = sizeof(A)/sizeof(A[0]);
-int yy = sizeof(A[0])/sizeof(A[0][0]);
 
 int main() {
+    int xx = sizeof(A)/sizeof(A[0]);
+    int yy = sizeof(A[0])/sizeof(A[0][0]);
     double *t_A;
-    double myresult = matrix_det((double *) &A[0], xx, yy);
-    printf("Hasil saya adalah %f\n\n\n",myresult);
-    matrix_print(&A[0],xx,yy);
+    double *t_B;
+    int *t_S;
+    //double myresult = matrix_det((double *) &A[0], xx, yy);
+    //printf("Hasil saya adalah %f\n\n\n",myresult);
+    //printf("apalah : %i and %i\n\n\n",xx,yy);
+    t_S=array_size((double *)A);
+    matrix_print((double *) &A[0],xx,yy);
+    printf("%i , %i \n\n",t_S[0],t_S[1]);
 
     printf("\n\n\n");
     //t_A = matrix_transpose((double *) &A[0], xx, yy);
-    t_A = matrix_identity(xx, yy);
+    //t_A = matrix_identity(xx, yy);
 
-    matrix_print(&t_A[0],xx,yy);
+    //matrix_print((double *) &t_A[0],xx,yy);
+
+    //t_B = matrix_gauss_jordan_elimination((double *) &A[0], xx, yy);
+
+    //matrix_print(&t_B[0],xx,yy);
 
     return 0;
 }
@@ -133,11 +148,22 @@ double *matrix_identity(int therow, int thecol) {
     }
     return idArray;
 }
+int *array_size(double *initArray) {
+    int *arraySize;
+    //int xx = sizeof(A)/sizeof(A[0]);
+    //int yy = sizeof(A[0])/sizeof(A[0][0]);
+    //*arraySize=sizeof(initArray)/sizeof(initArray[0]);
+    *arraySize=0;
+    //*(arraySize+1)=sizeof(initArray[0])/sizeof(initArray[0][0]);
+    *(arraySize+1)=0;
+    return arraySize;
+}
 /*
 * gaus jordan elimination
 */
-double matrix_gauss_jordan_elimination(double *initArray, int therow, int thecol) {
-    double *gjArray, *iArray;
+double *matrix_gauss_jordan_elimination(double *initArray, int therow, int thecol) {
+    double *gjArray;
+    double *iArray;
     double m;
 
     iArray = matrix_identity(therow, thecol);
@@ -145,12 +171,17 @@ double matrix_gauss_jordan_elimination(double *initArray, int therow, int thecol
     for(int i=0;i<thecol;i++) {
         for(int j=0;j<therow;j++) {
             //elimination process take place except for the diagonal
-            if(i != j) m = - (*(idArray + (i*therow)+j)) / (*(idArray + (i*thecol)+i));
-            for ()
+            if(i != j) {
+                m = - (*(initArray + (i*therow)+j)) / (*(initArray + (i*thecol)+i));
+                for (int k=j;k<thecol;k++) {
+                    *(initArray+(j*therow)+k) = *(initArray+(j*therow)+k) + m*(*(initArray+(i*therow)+k));
+                    *(iArray+(j*therow)+k) = *(iArray+(j*therow)+k) + m*(*(iArray+(i*therow)+k));
+                }
+            }
         }
     }
 
-    return gjArray;
+    return iArray;
 }
 /*
 * return inverse of a matrix
